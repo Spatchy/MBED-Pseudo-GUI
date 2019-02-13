dcd17af60cfd52df60980b3d64403f22c88aabc0
Spatchy <spatchy@spatchy.net>
2019-02-13 00:52:40 +0000

    created classes

--- src/Gui.java
+++ src/Gui.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import shed.mbed.LCD;
+
 public class Gui {
+
+    ArrayList<GuiTab> tabs;
+    LCD lcd;
+    String topLevelString;
+
+    public Gui(ArrayList<GuiTab> tabs, LCD lcd)
+    {
+        this.tabs = tabs;
+        this.lcd = lcd;
+    }
+
+    public void displayTopLevel()
+    {
+        lcd.print(0, 0, "boo");
+        for(GuiTab tab : tabs){
+            topLevelString = "" + tab.getTitle() + "  ";
+        }
+    }
+
+    public void selectTab(String title)
+    {
+       String[] splitTabs = topLevelString.split(title);
+       topLevelString = splitTabs[0] + ">" + splitTabs[1];
+    }
 }
