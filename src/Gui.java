e32eee9221835c0559e9015b53a11d5b4efac723
Spatchy <spatchy@spatchy.net>
2019-02-13 02:01:04 +0000

    moved formation of topLevelString to constructor

--- src/Gui.java
+++ src/Gui.java
@@ -11,20 +11,20 @@ public class Gui {
     {
         this.tabs = tabs;
         this.lcd = lcd;
-        lcd.print(0,0,".");
-    }
 
-    public void displayTopLevel()
-    {
-        lcd.print(0, 0, topLevelString);
         for(GuiTab tab : tabs){
             topLevelString = topLevelString + tab.getTitle() + "  ";
         }
     }
 
+    public void displayTopLevel()
+    {
+        lcd.print(0, 0, topLevelString);
+    }
+
     public void selectTab(String title)
     {
-        topLevelString = topLevelString.replace(">", "");
-        topLevelString = topLevelString.replace(title, ">"+title);
+        topLevelString = topLevelString.replace("*", "");
+        topLevelString = topLevelString.replace(title, "*"+title);
     }
 }
